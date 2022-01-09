9e42510bd9baef3481419ef52f8627b652cb8857
Haoxiang-Hu <huhaoxiangwow@gmail.com>
2022-01-09 22:10:25 +0800

    Add detector

--- motion_detector.py
+++ motion_detector.py
@@ -1,4 +1,4 @@
-import cv2
+import cv2,time
 
 first_frame = None
 
@@ -18,9 +18,20 @@ while True:
     thresh_frame = cv2.threshold(delta_frame, 30, 255, cv2.THRESH_BINARY)[1]
     thresh_frame = cv2.dilate(thresh_frame, None, iterations=2)
 
+    #Contour Method
+    (cnts,_) = cv2.findContours(thresh_frame.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    for contour in cnts:
+        if cv2.contourArea(contour) < 1000:
+            continue
+    
+        (x,y,w,h) = cv2.boundingRect(contour)
+        cv2.rectangle(frame, (x,y), (x+w,y+h), (0,255,0), 3)
+
     cv2.imshow("Gray Frame", gray) #打开一个窗口显示捕获的画面，每次使用imshow都需要记得下面需要一个destroyAllWindows()关闭窗口
     cv2.imshow("Delta Frame", delta_frame)
     cv2.imshow("Threshold Frame", thresh_frame)
+    cv2.imshow("Color Frame", frame)
 
     key = cv2.waitKey(1)
 

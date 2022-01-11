de3b37554d80d4c6c7057f36051a9f2a9f27fe14
Haoxiang-Hu <huhaoxiangwow@gmail.com>
2022-01-11 13:32:20 +0800

    Fix the dark image problem

--- motion_detector.py
+++ motion_detector.py
@@ -4,9 +4,15 @@ first_frame = None
 
 video = cv2.VideoCapture(0)
 
+#Turn on the camera for few seconds.
+i=10
+while i>0:
+    i = i-1
+    check, frame_1 = video.read()
+
 while True:
     check, frame = video.read()
-
+    status = 0
     gray =cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) #把frame转化为一个图片
     gray =cv2.GaussianBlur(gray,(21,21),0)
 
@@ -23,9 +29,10 @@ while True:
     (cnts,_) = cv2.findContours(thresh_frame.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
     for contour in cnts:
-        if cv2.contourArea(contour) < 1000:
+        if cv2.contourArea(contour) < 10000:
             continue
-    
+        
+        status = 1
         (x,y,w,h) = cv2.boundingRect(contour)
         cv2.rectangle(frame, (x,y), (x+w,y+h), (0,255,0), 3)
 
@@ -39,6 +46,8 @@ while True:
 
     if key == ord('q'):
         break
+
+    print(status)
 
 video.release()
 cv2.destroyAllWindows()

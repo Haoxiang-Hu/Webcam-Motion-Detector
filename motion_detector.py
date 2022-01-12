2b2af1b6e849d1acdfe426fff501d37a26da122e
Haoxiang-Hu <huhaoxiangwow@gmail.com>
2022-01-12 15:12:16 +0800

    Add time method

--- motion_detector.py
+++ motion_detector.py
@@ -1,11 +1,15 @@
-import cv2,time
+import cv2
+import time
+from datetime import datetime
 
 first_frame = None
+status_list = []
+times = []
 
 video = cv2.VideoCapture(0)
 
 #Turn on the camera for few seconds.
-i=10
+i=100
 while i>0:
     i = i-1
     check, frame_1 = video.read()
@@ -27,14 +31,22 @@ while True:
 
     #Contour Method
     (cnts,_) = cv2.findContours(thresh_frame.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
+    
     for contour in cnts:
         if cv2.contourArea(contour) < 10000:
             continue
-        
+
         status = 1
+
         (x,y,w,h) = cv2.boundingRect(contour)
         cv2.rectangle(frame, (x,y), (x+w,y+h), (0,255,0), 3)
+
+    status_list.append(status)
+
+    if status_list[-1] == 1 and status_list[-2] == 0:
+        times.append(datetime.now())
+    if status_list[-1] == 1 and status_list[-2] == 1:
+        times.append(datetime.now())
 
     cv2.imshow("First Frame", first_frame)
     cv2.imshow("Gray Frame", gray) #打开一个窗口显示捕获的画面，每次使用imshow都需要记得下面需要一个destroyAllWindows()关闭窗口
@@ -47,7 +59,9 @@ while True:
     if key == ord('q'):
         break
 
-    print(status)
+print(status_list)
+print(times)
 
-video.release()
+video.release()  
+
 cv2.destroyAllWindows()
